cfb7106442a4ba46e24af41122c6b94346477132
Carlosvl273 <valenciamendoza2004@gmail.com>
2026-01-13 20:39:04 -0500

    Sonnarq

--- vite.config.ts
+++ vite.config.ts
@@ -10,6 +10,11 @@ export default defineConfig({
     env: {
       VITE_SUPABASE_URL: 'https://iiqxgttmoxnxhjefnggw.supabase.co',
       VITE_SUPABASE_ANON_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImlpcXhndHRtb3hueGhqZWZuZ2d3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjE2MTU2MDYsImV4cCI6MjA3NzE5MTYwNn0.xwquuyR5Qp18_tq8nk4zeo3RAhV_hrCQNiYMwclXtcM'
+    },
+    coverage: {
+      provider: 'v8',
+      reporter: ['text', 'lcov', 'html'],
+      reportsDirectory: './coverage',
     }
   },
 })
